9bce75ed25b3a70af34b4884cd70403d42259627
Zach Starr <zach@starr.biz>
2021-01-08 14:15:03 -0600

    testing git ignore

--- Pathfinder1eCharacterSheet/src/app/app-routing.module.ts
+++ Pathfinder1eCharacterSheet/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CharacterSheetComponent } from './character-sheet/character-sheet.component';
+import { HomeComponent } from './home/home.component';
 
 
 const routes: Routes = [
-  { path: '**', component: CharacterSheetComponent }
+  { path: 'characters', component: CharacterSheetComponent },
+  { path: '**', component: HomeComponent }
 ];
 
 @NgModule({
--- Pathfinder1eCharacterSheet/src/app/app.module.ts
+++ Pathfinder1eCharacterSheet/src/app/app.module.ts
@@ -41,11 +41,49 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatTooltipModule} from '@angular/material/tooltip';
 import {MatTreeModule} from '@angular/material/tree';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
+
+import {firebase, firebaseui, FirebaseUIModule} from 'firebaseui-angular';
+import {environment} from '../environments/environment';
+
+import {AngularFireModule} from '@angular/fire';
+import {AngularFireAuthModule} from '@angular/fire/auth';
+
+const firebaseUiAuthConfig: firebaseui.auth.Config = {
+  signInFlow: 'popup',
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    {
+      scopes: [
+        'public_profile'//,
+        // 'email',
+        // 'user_likes',
+        // 'user_friends'
+      ],
+      customParameters: {
+        'auth_type': 'reauthenticate'
+      },
+      provider: firebase.auth.FacebookAuthProvider.PROVIDER_ID
+    },
+    firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+    firebase.auth.GithubAuthProvider.PROVIDER_ID,
+    {
+      requireDisplayName: false,
+      provider: firebase.auth.EmailAuthProvider.PROVIDER_ID
+    },
+    firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+    firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
+  ],
+  tosUrl: '<your-tos-link>',
+  privacyPolicyUrl: '<your-privacyPolicyUrl-link>',
+  credentialHelper: firebaseui.auth.CredentialHelper.GOOGLE_YOLO
+};
 
 @NgModule({
   declarations: [
     AppComponent,
-    CharacterSheetComponent
+    CharacterSheetComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
