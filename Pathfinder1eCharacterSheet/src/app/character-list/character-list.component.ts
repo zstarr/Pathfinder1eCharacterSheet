9521d6be00129f2d1464cb51a0bdb73d35419003
Zach Starr <zach@starr.biz>
2021-01-15 17:57:14 -0600

    Adding in real time db connection

--- Pathfinder1eCharacterSheet/src/app/character-list/character-list.component.ts
+++ Pathfinder1eCharacterSheet/src/app/character-list/character-list.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { Character } from '../core/models/character.model';
 import { CharacterService } from '../core/services/character.service';
 import { MatSidenav } from '@angular/material/sidenav';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-character-list',
@@ -16,9 +17,15 @@ export class CharacterListComponent implements OnInit {
 
   constructor(
     private characterService: CharacterService,
-    private router: Router
+    private router: Router,
+    private afAuth: AngularFireAuth
     ) {
-    this.characterService.characters.subscribe(chars => this.characters = chars);
+      this.afAuth.authState.subscribe(authState => {
+      if(authState?.uid) {
+        characterService.updateCharacters(authState?.uid);
+      }
+    });
+      this.characterService.characters.subscribe(chars => this.characters = chars);
   }
 
   ngOnInit(): void {
--- Pathfinder1eCharacterSheet/src/app/core/services/character.service.ts
+++ Pathfinder1eCharacterSheet/src/app/core/services/character.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { Character } from '../models/character.model';
-import { chars } from "./temp.data";
+import firebase from 'firebase/app';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CharacterService {
-
-  characters = new BehaviorSubject<Character[]>(chars);
+  private characterDbString: string;
 
   private character = new BehaviorSubject<Character>(null);
 
@@ -16,10 +16,41 @@ export class CharacterService {
 
   characterIds: number[];
 
-  constructor() { }
+  database: AngularFireDatabase;
+  user: firebase.User;
+
+  characters: BehaviorSubject<Character[]> = new BehaviorSubject<Character[]>(
+    null
+  );
+
+  constructor(private db: AngularFireDatabase)
+  {
+    this.database = db;
+  }
 
   loadCharacter(newCharacter: Character) {
     this.character.next(newCharacter);
+    console.log(newCharacter);
+    this.saveCharacter(newCharacter);
   }
 
+  updateCharacters(uid: string) {
+    this.characterDbString = 'users/' + uid + '/pathfinder1echaractersheets';
+    this.database
+      .list<Character>(this.characterDbString)
+      .valueChanges()
+      .subscribe((chars) => {
+        this.characters.next(chars);
+      });
+  }
+
+  saveCharacter(character: Character) {
+    var charTable = this.database.object(
+      this.characterDbString + '/' + character.id
+    );
+    charTable
+      .update(character)
+      .then(() => console.log('character submitted'))
+      .catch((error) => console.log('error saving char: ', error));
+  }
 }
